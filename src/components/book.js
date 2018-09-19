729ac6882589f83666e055ca74d50716fde371f8
Jennifer-Hanson <jennkhanson@gmail.com>
2018-09-18 21:26:47 -0400

    book details are dynamic

--- src/components/book.js
+++ src/components/book.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 export default class Book extends Component {
   render() {
+    const { title, author, bookCover } = this.props.book;
+    const { width, height, image } = bookCover;
     return (
       <div>
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
+            <div className="book-cover" style={{ width: width, height: height, backgroundImage: image }}></div>
             <div className="book-shelf-changer">
               <select>
                 <option value="move" disabled>Move to...</option>
@@ -18,8 +20,8 @@ export default class Book extends Component {
               </select>
             </div>
           </div>
-          <div className="book-title">1776</div>
-          <div className="book-authors">David McCullough</div>
+          <div className="book-title">{title}</div>
+          <div className="book-authors">{author}</div>
         </div>
       </li>
       </div>
